7bd0ccc575ff71ed9343e017c8c397114ba426aa
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-07-04 17:28:42 -0500

    Add a scrollable view

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 
 import ListItem from "../ListItem/ListItem";
 
@@ -12,7 +12,7 @@ const placeList = props => {
     />
   ));
 
-  return <View style={style.listContainer}>{placesOutput}</View>;
+  return <ScrollView style={style.listContainer}>{placesOutput}</ScrollView>;
 };
 
 const style = StyleSheet.create({
